a4591133c0dfda4411f663474a985f889046b67c
qqwq3 <1150783525@qq.com>
2019-12-17 14:41:39 +0800

    0

---
+++ index.js
@@ -0,0 +1,11 @@
+import HvrCurlTopLeft from './src/components/hvr-curl-top-left';
+
+export {
+  HvrCurlTopLeft,
+}
+
+export default {
+  install(Vue) {
+    Vue.component('hvr-curl-top-left', HvrCurlTopLeft);
+  }
+}
--- src/components/hvr-curl-top-left.vue
+++ src/components/hvr-curl-top-left.vue
@@ -1,10 +1,27 @@
 <template>
-    <button type="button" class="buts hvr-overline-from-left">点击我</button>
+    <button type="button" @click="click" :style="styles" class="buts hvr-overline-from-left">{{ text }}</button>
 </template>
 
 <script>
     export default {
-        name: "hvr-curl-top-left"
+        name: "hvr-curl-top-left",
+        props: {
+            // 按钮样式
+            styles: {
+                type: Object,
+                default: () => {}
+            },
+            // 按钮点击事件
+            click: {
+                type: Function,
+                default: () => null
+            },
+            // 按钮文字
+            text: {
+                type: String,
+                default: () => '我是按钮'
+            }
+        }
     }
 </script>
 
