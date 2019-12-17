135389d34589de65477c4e221e9a25f44cc7c762
qqwq3 <1150783525@qq.com>
2019-12-17 14:46:14 +0800

    0

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
-    <hvr-curl-top-left/>
+
   </div>
 </template>
 
 <script>
-  import hvrCurlTopLeft from './components/hvr-curl-top-left';
 
 export default {
-  name: 'App',
-  components: {
-      'hvr-curl-top-left': hvrCurlTopLeft
-  }
+  name: 'App'
 }
 </script>
 
 <style>
-  @import "./styles/index.css";
-  @import "./styles/global.css";
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import "./styles/index.css";
+import "./styles/global.css";
 
 Vue.config.productionTip = false;
 
