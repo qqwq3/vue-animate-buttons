1e8286b572cd673b8044f9de30d752c9c32cfc6f
qqwq3 <1150783525@qq.com>
2019-12-17 11:32:14 +0800

    init、

---
+++ src/components/hvr-curl-top-left.vue
@@ -0,0 +1,13 @@
+<template>
+    <button type="button" class="buts hvr-overline-from-left">点击我</button>
+</template>
+
+<script>
+    export default {
+        name: "hvr-curl-top-left"
+    }
+</script>
+
+<style scoped>
+
+</style>
