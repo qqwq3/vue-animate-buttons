ca3b2cf6c5488db645e8bc0cb09a63b9eba6fd95
qqwq3 <1150783525@qq.com>
2019-12-19 14:09:58 +0800

    0

---
+++ index.js
@@ -0,0 +1,11 @@
+import ButtonHvrCurlTopLeft from './src/hvr-curl-top-left';
+
+export {
+    ButtonHvrCurlTopLeft,
+}
+
+export default {
+    install (Vue) {
+        Vue.component(ButtonHvrCurlTopLeft);
+    }
+};
